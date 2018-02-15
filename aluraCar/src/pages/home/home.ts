c12ee063cf0c8df3c3e9e1f60c2eca6c33b470ab
André Luiz Damasceno <andre.luiz@eits.com.br>
2018-02-15 09:21:14 -0200

    first commit

---
+++ aluraCar/src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  url: string = 'http://aluracar.herokuapp.com/';
+
+
+  public carros;
+
+  constructor(public navCtrl: NavController, private _http: Http) 
+  {
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => { this.carros = carros 
+        console.log(this.carros)});
+
+  }
+
+}
